cf4e11e9ab13cee3c8d8a43150186b20ddcbf6a4
pedrocaah <pdrhenriqe@outlook.com>
2024-07-30 21:19:44 -0300

    commit module Desafio

--- Desafio/src/application/Program.java
+++ Desafio/src/application/Program.java
@@ -1,22 +1,26 @@
 package application;
 
-import service.SalarioService;
-
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        double valorImposto;
         System.out.println("Digite o valor bruto");
         double valorBruto = sc.nextDouble();
         System.out.println("Digite o valor dos beneficios");
         double valorBeneficios = sc.nextDouble();
 
-        SalarioService salarioService = new SalarioService();
-        salarioService.calculoSalario(valorBruto, valorBeneficios);
-        System.out.println(salarioService);
-
+        if (valorBruto <= 1100.00) {
+            valorImposto = 0.05 * valorBruto;
+        } else if (valorBruto < 2500.00) {
+            valorImposto = 0.1 * valorBruto;
+        } else {
+            valorImposto = 0.15 * valorBruto;
+        }
+        double salarioLiquido = valorBruto - valorImposto + valorBeneficios;
+        System.out.printf("Salario líquido é: %.2f", salarioLiquido);
         sc.close();
     }
 }
+
