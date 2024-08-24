d48c55bf95234315029dbbe744fef94906f1c5d6
Pedro Oliveira <pdrhenriqe@outlook.com>
2024-08-23 22:54:10 -0300

    arrumando ultimo commit

--- AbstraindoBootcamp/src/Cursos.java
+++
@@ -1,5 +0,0 @@
-public class Cursos extends AtividadesDoBootcamp{
-    public Cursos(String titulo, String descricao, String cargaHoraria) {
-        super(titulo, descricao, cargaHoraria);
-    }
-}
--- AbstraindoBootcamp/src/Mentorias.java
+++
@@ -1,9 +0,0 @@
-public class Mentorias extends AtividadesDoBootcamp{
-
-
-    public Mentorias(String titulo, String descricao, String cargaHoraria) {
-        super(titulo, descricao, cargaHoraria);
-    }
-
-}
-
